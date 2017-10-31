9a4f151356fac4a7c2af7114f25d6dbbb17a897b
cy123321 <729968075@qq.com>
2017-10-31 19:01:22 +0800

    completion of problem 1003

--- pku_oj/pku_oj/id_1003.cpp
+++ pku_oj/pku_oj/id_1003.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 int main()
 {
-	float s, t;
+	float s, t,num;
 
+	while (cin >> s, s != 0)
+	{
+		//cout << s << endl;
+		t = 0;
+		num = 0;
+		while (t < s) 
+		{
+			//cout << t << endl;
+			//cout << num << endl;
+			num++;
+			t += 1.0 / (num+1);
+		}
+		cout << num << " card(s)\n";
+	}
+	
 	return 0;
 }
